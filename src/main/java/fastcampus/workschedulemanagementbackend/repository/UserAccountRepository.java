32947a450054c2240e90d59c312f43d5a991ff84
itstimi-XD <ssk0710402@gmail.com>
2023-03-23 19:37:57 +0900

    #10- Create UserAccountRepository.java

--- src/main/java/fastcampus/workschedulemanagementbackend/repository/UserAccountRepository.java
+++ src/main/java/fastcampus/workschedulemanagementbackend/repository/UserAccountRepository.java
@@ -2,6 +2,12 @@ package fastcampus.workschedulemanagementbackend.repository;
 
 import fastcampus.workschedulemanagementbackend.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+
+Optional<UserAccount> findById(String id);
 }
