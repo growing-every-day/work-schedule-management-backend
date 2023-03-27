420a815369ee29b9e6611f75aa7a80bcc087e208
chan <seulchan07@gmail.com>
2023-03-27 22:16:14 +0900

    feat #13 - 회원정보 API 테스트하기 위한 초기 회원 데이터 생성 및 JPA, Security 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+-- 테스트 계정
+-- TODO: 테스트용 회원 추가하기
+insert into user_account (id, username, password, name, email, role, remained_vacation_count, created_at,
+                          modified_at)
+values (2, 'chan', '{noop}chan', 'chan', 'chan@gmail.com', 'USER', 25, now(), now())
+;
+insert into user_account (id, username, password, name, email, role, remained_vacation_count, created_at,
+                          modified_at)
+values (1, 'admin', '{noop}admin', 'admin', 'admin@mail.com', 'ADMIN', 25,  now(), now())
+;
