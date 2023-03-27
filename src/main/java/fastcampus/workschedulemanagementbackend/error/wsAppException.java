187df407908a9eaf55e0431ae376ace193504416
itstimi-XD <ssk0710402@gmail.com>
2023-03-28 03:11:21 +0900

    #10 - ErrorHandler updated
    wsAppException을 BadRequestException에 병합하려했으나 실패하여 그냥 ErrorHandler만 수정

--- src/main/java/fastcampus/workschedulemanagementbackend/error/wsAppException.java
+++ src/main/java/fastcampus/workschedulemanagementbackend/error/wsAppException.java
@@ -1,5 +1,21 @@
 package fastcampus.workschedulemanagementbackend.error;
 
-// TODO : implement
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class wsAppException extends RuntimeException{
+
+    private ErrorCode errorCode;
+    private String message;
+
+    @Override
+    public String getMessage(){
+        if(message == null){
+            return errorCode.getMessage();
+        }
+
+        return String.format("%s. %s", errorCode.getMessage(), message);
+    }
 }
