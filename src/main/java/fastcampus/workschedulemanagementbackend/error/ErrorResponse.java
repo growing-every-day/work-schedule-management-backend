7a139b37d2c96972fe528eb7c95a9232403da859
chan <seulchan07@gmail.com>
2023-03-29 14:38:46 +0900

    feat #20 - 검증 로직 추가로 Exception 생성, ErrorHandler, ErrorResponse 수정

--- src/main/java/fastcampus/workschedulemanagementbackend/error/ErrorResponse.java
+++ src/main/java/fastcampus/workschedulemanagementbackend/error/ErrorResponse.java
@@ -1,9 +1,17 @@
 package fastcampus.workschedulemanagementbackend.error;
 
-public record ErrorResponse(Integer errorCode, String errorMessage) {
+import java.util.List;
+
+public record ErrorResponse(Integer errorCode, String errorMessage, List fieldErrors) {
 
     public static ErrorResponse of(Integer errorCode, String errorMessage) {
-        return new ErrorResponse(errorCode, errorMessage);
+        return new ErrorResponse(errorCode, errorMessage, null);
     }
+
+    public static ErrorResponse of(Integer errorCode, String errorMessage, List fieldErrors) {
+        return new ErrorResponse(errorCode, errorMessage, fieldErrors);
+    }
+
+
 
 }
