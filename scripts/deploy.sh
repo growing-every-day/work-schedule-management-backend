b7f1ffe2fd5ad181b3b7db6847500d7ddece40ad
Chan <seulchan07@gmail.com>
2023-03-30 14:37:23 +0900

    deploy할 때 현재 구동 중인 애플리케이션 찾지 못하는 버그 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,14 +4,14 @@ REPOSITORY=/home/ec2-user/app
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
+CURRENT_PID=$(pgrep -fla java | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
   echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
+  echo "> sudo kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
