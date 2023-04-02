b06c9751d404dab10f6eda014c422d53715fccf0
chan <seulchan07@gmail.com>
2023-04-02 16:04:29 +0900

    feat #47 회원가입시 보내주는 응답 형식 변경

--- src/main/java/fastcampus/workschedulemanagementbackend/dto/response/useraccount/UserAccountDeleteResponse.java
+++ src/main/java/fastcampus/workschedulemanagementbackend/dto/response/useraccount/UserAccountDeleteResponse.java
@@ -2,7 +2,7 @@ package fastcampus.workschedulemanagementbackend.dto.response.useraccount;
 
 public record UserAccountDeleteResponse(Boolean deleted) {
 
-        public static UserAccountDeleteResponse of(Boolean deleted) {
-            return new UserAccountDeleteResponse(deleted);
-        }
+    public static UserAccountDeleteResponse of(Boolean deleted) {
+        return new UserAccountDeleteResponse(deleted);
+    }
 }
